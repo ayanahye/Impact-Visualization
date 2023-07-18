991d295bc2b31d93db7096b858d5db9ea65a00f1
ayanahye <ayanahye2@gmail.com>
2023-07-18 16:34:28 -0700

    got data

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
+# data taken from: https://www2.gov.bc.ca/gov/content/safety/wildfire-status/about-bcws/wildfire-statistics
+
+
 import plotly.express as px
 import pandas as pd
 
-print("getting data...")
-df = pd.DataFrame({
-    'longitude': [-122.5, -123.4, -122.9],
-    'latitude': [45.5, 45.2, 45.7],
-    'burned_area': [500, 1000, 750],
-    'year': [2010, 2015, 2020],
-    'temperature': [20, 22, 25]
-})
+#using different colors or symbols to differentiate between different animal species.
+
+print("Getting data...")
+df = pd.read_csv("data_part.csv")
+
+# convert latitude and longitude from degrees and minutes to decimal degrees
+df['latitude'] = df['latitude'].apply(lambda x: int(x.split()[0]) + float(x.split()[1]) / 60)
+df['longitude'] = df['longitude'].apply(lambda x: -(int(x.split()[0]) + float(x.split()[1]) / 60))  # Multiply by -1 to make longitudes negative
+
+# save the updated dataframe back to the CSV file
+df.to_csv("updated_data_part.csv", index=False)
 
 print(df.head(10))
 print(df.tail(10))
 
-# Create a scatter mapbox plot
-fig = px.scatter_mapbox(df, 
-                        lat=df['latitude'], 
-                        lon=df['longitude'], 
-                        color=df['burned_area'],
-                        color_continuous_scale='Reds', 
-                        size=df['burned_area'], 
-                        animation_frame='year',
-                        zoom=9, 
-                        width=800, 
-                        height=750, 
+# create a scatter mapbox plot
+fig = px.scatter_mapbox(df,
+                        lat='latitude',
+                        lon='longitude',
+                        color='Year',
+                        color_continuous_scale='Reds',
+                        size='Size',
+                        animation_frame='Year',
+                        zoom=9,
+                        width=800,
+                        height=750,
                         title="Forest Fire Impact Map"
                         )
 
 
 fig.update_layout(mapbox_style="open-street-map")
-fig.update_layout(margin={"r":0, "t":50, "b":10})
+fig.update_layout(margin={"r": 0, "t": 50, "b": 10})
 fig.show()
 
-print("plot complete.")
+print("Plot complete.")
 
