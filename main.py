eee0226360252651f37306d7c857d9cfcf9a0171
ayanahye <ayanahye2@gmail.com>
2023-07-18 16:46:05 -0700

    modified data structure, changed map

--- main.py
+++ main.py
@@ -7,14 +7,7 @@ import pandas as pd
 #using different colors or symbols to differentiate between different animal species.
 
 print("Getting data...")
-df = pd.read_csv("data_part.csv")
-
-# convert latitude and longitude from degrees and minutes to decimal degrees
-df['latitude'] = df['latitude'].apply(lambda x: int(x.split()[0]) + float(x.split()[1]) / 60)
-df['longitude'] = df['longitude'].apply(lambda x: -(int(x.split()[0]) + float(x.split()[1]) / 60))  # Multiply by -1 to make longitudes negative
-
-# save the updated dataframe back to the CSV file
-df.to_csv("updated_data_part.csv", index=False)
+df = pd.read_csv("general_data_part.csv")
 
 print(df.head(10))
 print(df.tail(10))
@@ -27,14 +20,14 @@ fig = px.scatter_mapbox(df,
                         color_continuous_scale='Reds',
                         size='Size',
                         animation_frame='Year',
-                        zoom=9,
-                        width=800,
+                        zoom=4,
+                        width=1000,
                         height=750,
                         title="Forest Fire Impact Map"
                         )
 
 
-fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(mapbox_style="carto-darkmatter")
 fig.update_layout(margin={"r": 0, "t": 50, "b": 10})
 fig.show()
 
