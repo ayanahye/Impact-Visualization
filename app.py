884a7e4a44f7e5cf6f2d589d9da36cb3c2743c1a
ayanahye <ayanahye2@gmail.com>
2023-07-22 16:01:50 -0700

    made ui prettyyyy

--- app.py
+++ app.py
@@ -3,23 +3,20 @@
 # carbon data
 #https://cfs.nrcan.gc.ca/statsprofile/carbon
 import plotly.express as px
+import plotly.express as px
 import pandas as pd
+import plotly.graph_objects as go
+import dash
+from dash import html, dcc, dash_table
 
-from flask import Flask, render_template
-
-app = Flask(__name__)
+app = dash.Dash(__name__)
 
 def create_plot():
     print("Getting data...")
     df = pd.read_csv("general_data_part.csv")
 
-    additional_data = pd.read_csv("CO2_data.csv")
-
-    # Merge the forest fire data and additional data based on the common column (e.g., year)
-    merged_df = pd.merge(df, additional_data, on='Year')
-
     # Create a scatter mapbox plot
-    fig = px.scatter_mapbox(merged_df,
+    fig = px.scatter_mapbox(df,
                             lat='latitude',
                             lon='longitude',
                             color='Year',
@@ -27,31 +24,53 @@ def create_plot():
                             size='Size',
                             animation_frame='Year',
                             zoom=4,
-                            width=1000,
                             height=750,
                             title="Forest Fire Impact Map",
                             hover_name='Geographic',
-                            hover_data={
-                                'Size': ':.2f',
-                                'Area of managed forests (hectares)': True,
-                                'Total net emissions or removals to the atmosphere, all causes (CO2e/yr, megatonnes)': True,
-                                'Net emissions or removals due to natural disturbances (CO2e/yr, megatonnes)': True,
-                                'Net emissions or removals due to human forest management activities and from harvested wood products (CO2e/yr, megatonnes)': True,
-                                'Transfers from the managed forest sector to the forest products sector due to harvesting (CO2e/yr, megatonnes)': True
-                            }
                             )
 
     fig.update_layout(mapbox_style="carto-darkmatter")
     fig.update_layout(margin={"r": 0, "t": 50, "b": 10})
+    fig.update_layout(plot_bgcolor='grey', paper_bgcolor='grey')  
+
+    # Set tickvals for the color bar legend to show only whole numbers for the year
+    fig.update_coloraxes(colorbar_tickvals=sorted(df['Year'].unique(), reverse=True))
 
     return fig
 
-@app.route('/')
-def index():
-    fig = create_plot()
-    return render_template('index.html', plot=fig.to_html())
+def create_table():
+    co2_data = pd.read_csv("CO2_data.csv")
+    return dash_table.DataTable(
+        id='co2-table',
+        columns=[{'name': col, 'id': col} for col in co2_data.columns],
+        data=co2_data.to_dict('records'),
+        style_table={
+            'height': '400px',  # Set the table height
+            'width': '100%',
+            'overflowY': 'auto',  # Add scrollbars when content overflows
+            'margin': '0 auto',  # Center the table horizontally
+            'backgroundColor': 'grey',  
+        },
+        style_header={
+            'backgroundColor': 'rgb(230, 230, 230)',  
+            'fontWeight': 'bold'  
+        },
+        style_cell={
+            'textAlign': 'center',  
+            'minWidth': '100px',  
+            'backgroundColor': 'grey'  
+        }
+    )
+
+
+app.layout = html.Div(style={'backgroundColor': 'darkgrey', 'width': '100vw', 'height': '100vh'},
+                      children=[
+                          html.H1("Forest Fire Impact Map", style={'textAlign': 'center', 'color': 'white'}),
+                          dcc.Graph(figure=create_plot(), style={'width': '100%'}),  
+                          html.H2("CO2 Data Summary", style={'backgroundColor': 'darkgrey','textAlign': 'center', 'color': 'white'}),
+                          create_table()
+                      ])
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
-print("Plot complete.")
+    app.run_server(debug=True)
+
